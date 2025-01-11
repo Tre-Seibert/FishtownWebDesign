55ff2f06228e84fca01885b74c61166c61e4facc
Tre Seibert <treseibert3@gmail.com>
2025-01-11 16:55:20 -0500

    v0.9.8-beta

--- post.config.js
+++ post.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require('@fullhuman/postcss-purgecss')({
-      content: ['public/**/*.html', 'public/js/**/*.js'], // scan all HTML and JS files
+      content: ['public/**/*.html', 'public/js/**/*.js', 'node_modules/bootstrap/dist/css/bootstrap.min.css'], // scan all HTML and JS files
       safelist: ['safelist-class'], // optional: list any classes you want to keep
     }),
     require('cssnano')({
