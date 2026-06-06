072554c99fbce4f6723cf71799e764cc0885fd22
Tre Seibert <63426280+Tre-Seibert@users.noreply.github.com>
2026-06-06 16:09:28 -0400

    2.5.1

---
+++ src/js/swiper-custom.js
@@ -0,0 +1,6 @@
+import Swiper from 'swiper';
+import { Pagination, Autoplay, EffectCoverflow } from 'swiper/modules';
+
+Swiper.use([Pagination, Autoplay, EffectCoverflow]);
+
+window.Swiper = Swiper;
